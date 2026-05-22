a08aeaf296a4130db26b2e191d92695e4e838d93
Sarvesh S <sarvesh@candata.tech>
2026-05-22 15:27:32 +0530

    Build error - Fixed

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -183,6 +183,7 @@ export default function ProfilePage() {
             label='Current Password'
             value={currentPassword}
             onChange={(e) => setCurrentPassword(e.target.value)}
+            isRequired={false}
             classes='!bg-gray-50 !shadow-none'
           />
           <TextField
@@ -190,6 +191,7 @@ export default function ProfilePage() {
             label='New Password'
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
+            isRequired={false}
             classes='!bg-gray-50 !shadow-none'
           />
           <TextField
@@ -197,6 +199,7 @@ export default function ProfilePage() {
             label='Confirm New Password'
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
+            isRequired={false}
             classes='!bg-gray-50 !shadow-none'
           />
 
